a94dcf9f04759047cdf051aed4799c21c2f50a01
Joaquim Tomas <joaquimemtomas@gmail.com>
2021-03-17 15:01:52 +0000

    Criação do Projeto base ASP .NET com autenticação

---
+++ Luiza Andaluz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Luiza_Andaluz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
