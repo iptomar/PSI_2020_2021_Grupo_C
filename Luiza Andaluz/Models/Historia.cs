b872522959d93a6db1a56436560ffe9b2da3f956
Pedro Pires <aluno21215@ipt.pt>
2021-04-16 10:58:19 +0100

    Algumas Expressões Regulares (Models/Historia.cs)

--- Luiza Andaluz/Models/Historia.cs
+++ Luiza Andaluz/Models/Historia.cs
@@ -14,20 +14,37 @@ namespace LuizaAndaluz.Models
     {
         [Key]
         public String ID { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "O Título é de preenchimento obrigatório.")]
+        [Display(Name = "Título")]
         public String Titulo { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "A Descrição é de preenchimento obrigatório.")]
+        [Display(Name = "Descrição")]
         public String Descricao { get; set; }
 
         public bool Estado { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório.")]
+        [StringLength(40, ErrorMessage = "O {0} não pode ter mais de {1} carateres.")]
+        [RegularExpression("[A-ZÂÓÍÉ][a-záéíóúàèìòùâêîôûãôûäëïöüçñ]+(( | d[oa](s)? | (d)?e |-|'| d')[A-ZÂÓÍÉ][a-záéíóúàèìòùâêîôûãôûäëïöüçñ]+){1,3}",
+         ErrorMessage = "Só são aceites letras.<br />A primeira letra de cada nome é uma Maiúscula seguida de letras minúsculas.<br />Deve escrever entre 2 e 4 nomes.")]
         public String Nome { get; set; }
 
+
+        [Required(ErrorMessage = "A Idade é de preenchimento obrigatório.")]
+        [RegularExpression("[1-9][0-9]", ErrorMessage = "A idade deve ser entre 10 e 99 anos")]
         public int Idade { get; set; }
-        [Required]
+
+
+        [Required(ErrorMessage = "O Email é de preenchimento obrigatório.")]
         public String Email { get; set; }
 
+
         public DateTime Data { get; set; }
+
 
         public String Validador { get; set; }
         /// <summary>
