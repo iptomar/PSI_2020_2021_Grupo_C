9671395fd14eb3b41fee03905b3e78a4ce121245
Ricardo Vieira <62380583+ricvigh@users.noreply.github.com>
2021-04-16 02:30:12 +0100

    Configuração inicial Localization e Globalization.

--- Luiza Andaluz/Controllers/HomeController.cs
+++ Luiza Andaluz/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using Luiza_Andaluz.Data;
 using Luiza_Andaluz.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -15,22 +19,39 @@ namespace Luiza_Andaluz.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _stringLocalizer;
 
-        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger, IStringLocalizer<HomeController> stringLocalizer)
         {
             _context = context;
             _logger = logger;
+            _stringLocalizer = stringLocalizer;
         }
 
         public async Task<IActionResult> Index()
         {
+            /*string test = _stringLocalizer["Boas"].Value;
+            ViewData["Title"] = test;*/
+            string message = _stringLocalizer["GreetingMessage"].Value;
+            ViewData["Title"] = message;
             var applicationDbContext = _context.Historias.Include(h => h.Local).Where(h => h.Estado == true);
             ViewBag.locais = applicationDbContext.Select(x => x.Local).ToList();
             return View(await applicationDbContext.ToListAsync());
         }
 
+        [HttpPost]
+        public IActionResult GereLinguagens(string linguagem)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(linguagem)),
+                new CookieOptions { Expires = DateTime.Now.AddHours(12) });
+                
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Privacy()
         {
+            string message = _stringLocalizer["GreetingMessage"].Value;
+            ViewData["Title"] = message;
             return View();
         }
 
--- Luiza Andaluz/Startup.cs
+++ Luiza Andaluz/Startup.cs
@@ -4,12 +4,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,11 +43,25 @@ namespace Luiza_Andaluz
                 .AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
+            services.AddControllersWithViews();
+            services.AddLocalization(opt => { opt.ResourcesPath = "Resources"; });
+            services.Configure<RequestLocalizationOptions>(options => {
+                List<CultureInfo> supportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("pt-PT"),
+                    new CultureInfo("en-GB")
+                };
+                options.DefaultRequestCulture = new RequestCulture("pt-PT");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -62,6 +80,16 @@ namespace Luiza_Andaluz
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            /*var supportedCultures = new[] { "pt", "en", "es" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(localizationOptions);*/
+
+            //app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+            var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
 
             app.UseEndpoints(endpoints =>
             {
