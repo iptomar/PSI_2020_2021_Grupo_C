0aa1b068cad78187e3acaded6468168914ba80f3
Afonso Seguro <afonsoseguro@sapo.pt>
2021-05-25 16:18:27 +0100

    data de nascimento

---
+++ Luiza Andaluz/Data/Migrations/20210525150849_DataNascimento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Luiza_Andaluz.Data.Migrations
+{
+    public partial class DataNascimento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Idade",
+                table: "Historias");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Historias",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNascimento",
+                table: "Historias",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "Historias");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Historias",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Idade",
+                table: "Historias",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
